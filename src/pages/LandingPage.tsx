6ea3ae31842383489cfd8310d43b49fdb5783c72
Gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-06-09 18:23:23 -0400

    ui-ux template

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,15 +7,33 @@ type LandingPageProps = {
 export default function LandingPage({onEnter,}: LandingPageProps) {
     return (
         <div className="landing">
-            <h1>Aurea</h1>
+            <div className="landing-card">
+                <h1 className="landing-title">
+                    Aurea
+                </h1>
 
-            <p>
-                Construct sacred geometry digitally using a virtual compass and straightedge.
-            </p>
+                <p className="landing-subtitle">
+                    Quickly prototype sacred geometric forms using
+                    a virtual compass and straightedge.
+                </p>
 
-            <button onClick={onEnter}>
-                Enter Workspace
-            </button>
+                {/*<div className="landing-info">*/}
+                {/*    <span>Delete/Backspace: Delete selected geometry</span>*/}
+                {/*    <span>Middle Mouse Btn: Pan canvas</span>*/}
+                {/*    <span>Scroll Wheel:     Zoom canvas</span>*/}
+                {/*    <span>Left Mouse Btn:   Use tool</span>*/}
+                {/*    <span>Right Mouse Btn:  Cancel tool usage</span>*/}
+                {/*</div>*/}
+
+                <div className="landing-actions">
+                    <button
+                        className="landing-button"
+                        onClick={onEnter}
+                    >
+                        Enter Workspace
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
