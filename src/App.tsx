4e47b1ac81007de80186d1d2fd7dd6767984a873
gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-06-28 18:55:48 -0400

    comment out landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import LandingPage from "./pages/LandingPage";
 import EditorPage from "./pages/EditorPage";
 
 export default function App() {
 
-    const [entered, setEntered] =
-        useState(false);
+    // const [entered, setEntered] =
+    //     useState(false);
 
-    if (!entered) {
-        return (
-            <LandingPage
-                onEnter={() => setEntered(true)}
-            />
-        );
-    }
+    // if (!entered) {
+    //     return (
+    //         <LandingPage
+    //             onEnter={() => setEntered(true)}
+    //         />
+    //     );
+    // }
 
     return <EditorPage />;
 }
