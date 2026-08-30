69e24cbed924ddd97c2125942ed3cd1ddf964e67
Gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-08-29 23:47:14 -0400

    nuke old landingpage code

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,33 +7,8 @@ type LandingPageProps = {
 export default function LandingPage({onEnter,}: LandingPageProps) {
     return (
         <div className="landing">
-            <div className="landing-card">
-                <h1 className="landing-title">
-                    Aurea
-                </h1>
-
-                <p className="landing-subtitle">
-                    Quickly prototype sacred geometric forms using
-                    a virtual compass and straightedge.
-                </p>
-
-                {/*<div className="landing-info">*/}
-                {/*    <span>Delete/Backspace: Delete selected geometry</span>*/}
-                {/*    <span>Middle Mouse Btn: Pan canvas</span>*/}
-                {/*    <span>Scroll Wheel:     Zoom canvas</span>*/}
-                {/*    <span>Left Mouse Btn:   Use tool</span>*/}
-                {/*    <span>Right Mouse Btn:  Cancel tool usage</span>*/}
-                {/*</div>*/}
-
-                <div className="landing-actions">
-                    <button
-                        className="landing-button"
-                        onClick={onEnter}
-                    >
-                        Enter Workspace
-                    </button>
-                </div>
-            </div>
+            <p>uh guys</p>
+            <button onClick={onEnter}>Enter Workspace</button>
         </div>
     );
 }
