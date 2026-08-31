6b1b9fb40c08683b004c9938c5eb75ea4894372a
Gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-08-30 21:17:32 -0400

    use hashrouter in lieu of browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./global.css"
-import { BrowserRouter } from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 
 
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <App></App>
-    </BrowserRouter>
+    </HashRouter>
   // </StrictMode>,
 )
