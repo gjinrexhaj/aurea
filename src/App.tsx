d9083a0829f574498f45de9d3c28a57fdcbeac45
gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-09-03 18:31:35 -0400

    add manual page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import EditorPage from "./pages/EditorPage";
 import LandingPage from "./pages/LandingPage.tsx";
-import { logger } from './util/Logger.ts';
 import {Routes, Route} from "react-router-dom";
+import ManualPage from './pages/ManualPage.tsx';
 
 export default function App() {
-
-    logger.info("Aurea: Digital sacred geometry construction software");
-    logger.info("Developed and maintained by Gjin Rexhaj");
 
     return (
         <Routes>
             <Route path="/" element={ <LandingPage/> }></Route>
             <Route path="/app" element={ <EditorPage/> }></Route>
+            <Route path="/manual" element={<ManualPage/>}></Route>
         </Routes>
     );
 }
