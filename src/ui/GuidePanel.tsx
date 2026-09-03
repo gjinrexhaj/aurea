4e052c6b325f1574eb14e369918459902b839cbd
gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-09-03 17:16:05 -0400

    compact details

--- src/ui/GuidePanel.tsx
+++ src/ui/GuidePanel.tsx
@@ -11,17 +11,21 @@ export function GuidePanel() {
 
         <div className="guide-section">
           <h3>Tool usage</h3>
-          <p>1: Select tool</p>
-          <p>2: Point tool</p>
-          <p>3: Compass tool</p>
-          <p>4: Line tool</p>
+          <p>
+            1: Select tool<br/>
+            2: Point tool<br/>
+            3: Compass tool<br/>
+            4: Line tool
+          </p>
         </div>
 
         <div className="guide-section">
           <h3>Canvas controls</h3>
-          <p>LMB or RMB: use tool</p>
-          <p>DEL: delete selected geometry</p>
-          <p>Use scroll wheel to zoom in/out</p>
+          <p>
+            LMB or RMB: use tool<br/>
+            DEL: delete selected geometry<br/>
+            Use scroll wheel to zoom in/out
+          </p>
         </div>
       </div>
     </div>
